8ee7f35d930c2e1507fc7a47ba5111e31b124afc
Shashwat <codesurgeonx@Shashwats-iMac.local>
2019-02-16 16:42:37 +0530

    MVC Restructuring

---
+++ Whats Next/Models/ItemModel.swift
@@ -0,0 +1,15 @@
+//
+//  ItemModel.swift
+//  Whats Next
+//
+//  Created by Shashwat  on 16/02/19.
+//  Copyright © 2019 Shashwat . All rights reserved.
+//
+
+import Foundation
+
+class ItemModel{
+    
+    var title : String = ""
+    var isDone : Bool = false
+}
--- Whats Next/ToDoMasterViewController.swift
+++ Whats Next/ToDoMasterViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class ToDoMasterViewController: UITableViewController {
     
-    var toDoArray = [String]()
+    var toDoArray = [ItemModel]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "ToDoList") as? [String]{
-            toDoArray = items
-        }
+        super.viewDidLoad()        
+//        if let items = defaults.array(forKey: "ToDoList") as? [ItemModel]{
+//            toDoArray = items
+//        }
+//
+        
+        var item = ItemModel()
+        item.isDone = true
+        item.title = "First Task"
+        toDoArray.append(item)
+        
+        
+        item = ItemModel()
+        item.isDone = false
+        item.title = "Second Task"
+        toDoArray.append(item)
+        //Check if the data you are getting isn't nil
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,30 +38,34 @@ class ToDoMasterViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Cell for row at called")
         let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myReuableCell", for: indexPath)
-        cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.textLabel?.text = (toDoArray[indexPath.row] as ItemModel).title
+        
+        let item = toDoArray[indexPath.row]
+        cell.accessoryType = item.isDone ? .checkmark : .none
+        //Dont use that table view method of cellforrowat, keep in mind cell is stillbeing created
         return cell
     }
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(toDoArray[indexPath.row])
+        toDoArray[indexPath.row].isDone = !toDoArray[indexPath.row].isDone
+        self.tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-        if         tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        //REMOVED CODE THAT USED TO SET ACCESSORY TYPE HERE
     }
     
     @IBAction func barButtonPressed(_ sender: UIBarButtonItem) {
         var tf = UITextField()
         let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            print("Success")
-            self.toDoArray.append(tf.text!)
+            let tempModel = ItemModel()
+            tempModel.title = tf.text!
+            tempModel.isDone = false
+            self.toDoArray.append(tempModel)
             
-            self.defaults.set(self.toDoArray, forKey: "ToDoList")
+//            self.defaults.set(self.toDoArray, forKey: "ToDoList")
             
             self.tableView.reloadData()   //Called when action is completed
         }
